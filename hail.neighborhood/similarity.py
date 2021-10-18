600296df116319b92ff5754a2227460c3f3c64e3
hail-kang <73281731+hail-kang@users.noreply.github.com>
2021-10-18 22:35:32 +0900

    코사인유사도 모듈 생성

--- hail.neighborhood/similarity.py
+++ hail.neighborhood/similarity.py
@@ -1,4 +1,9 @@
 import abc
+import numbers
+
+from numpy import dot
+from numpy.linalg import norm
+from numpy.lib.function_base import iterable
 
 class Similarity(metaclass=abc.ABCMeta):
 
@@ -18,4 +23,41 @@ class Similarity(metaclass=abc.ABCMeta):
   def report(self):
     pass
 
+class CosineSimilarity(Similarity):
 
+  def __init__(self, origin:dict):
+    if not isinstance(origin, dict):
+      raise Exception('origin must be dict')
+    elif not all(map(lambda x : isinstance(x, numbers.Number), origin.values())):
+      raise Exception('value of origin must be number')
+    self.origin = origin
+
+    self.__origin_keys = set(self.keys())
+    self.__other_keys = None
+    self.__origin_values = None
+    self.__other_values = None
+
+  def keys(self) -> iter:
+    return self.origin.keys()
+
+  def values(self) -> iter:
+    return self.origin.values()
+
+  def distance(self, other) -> float:
+    self.__other_keys = set(other.keys())
+    union_keys = self.__origin_keys | self.__other_keys
+    self.__origin_values = [self.origin[key] if key in self.__origin_keys else 0 for key in union_keys]
+    self.__other_values = [other[key] if key in self.__other_keys else 0 for key in union_keys]
+
+    d = (norm(self.__origin_values)*norm(self.__other_values))
+    if d == 0:
+      return 0
+    return dot(self.__origin_values, self.__other_values)/d
+
+  def report(self, other) -> dict:
+    distance = self.distance(other)
+    substract_keys = self.__other_keys - self.__origin_keys
+    return {
+      'distance': distance,
+      'key_value': { key: other[key] for key in substract_keys }
+    }

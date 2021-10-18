cf32121d313c5adea8885c5db4b9ececfa119fd7
hail-kang <73281731+hail-kang@users.noreply.github.com>
2021-10-18 23:50:07 +0900

    neighborhood를 인터페이스에서 클래스로 변경

--- hail.neighborhood/neighborhood.py
+++ hail.neighborhood/neighborhood.py
@@ -1,11 +1,18 @@
-import abc
+from .similarity import Similarity
 
-class Neighborhood(metaclass=abc.ABCMeta):
+class Neighborhood:
 
-  @abc.abstractclassmethod
-  def add(self):
-    pass
+  def __init__(self, similarity, threshold):
+    if not isinstance(similarity, Similarity):
+      raise Exception('similarity must be Similarity')
+    self.similarity = similarity
+    self.threshold = threshold
+    self.neighborhoods = []
 
-  @abc.abstractclassmethod
+  def add(self, other):
+    report = self.similarity.report(other)
+    if report['distance'] > self.threshold:
+      self.neighborhoods.append(report)
+
   def report(self):
-    pass
+    return self.neighborhoods

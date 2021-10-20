1d9518f33d50400f3e056cb9da702f97deafb094
hail-kang <hail1225@naver.com>
2021-10-20 09:23:22 +0900

    단위테스트 테스트케이스 작성

--- test.py
+++ test.py
@@ -1,10 +1,18 @@
+import unittest
+
 from neighborhood import *
 
-s = CosineSimilarity({'a': 2, 'b': 1, 'c': 5})
-n = Neighborhood(s, 0.5)
-r = RatioRecommend(n, 3)
-r.add({'a': 2, 'b': 1, 'c': 5, 'd': 4})
-r.add({'a': 3, 'b': 1, 'c': 5, 'd': 4})
-r.add({'a': 2, 'b': 2, 'c': 5, 'd': 3})
-p = r.report(0.5)
-print(p)
+class RecommendTest(unittest.TestCase):
+
+  def test_cosine_smallcase(self):
+    s = CosineSimilarity({'a': 2, 'b': 1, 'c': 5})
+    n = Neighborhood(s, 0.5)
+    r = RatioRecommend(n)
+    r.add({'a': 2, 'b': 1, 'c': 5, 'd': 4})
+    r.add({'a': 3, 'b': 1, 'c': 5, 'd': 4})
+    r.add({'a': 2, 'b': 2, 'c': 5, 'd': 3})
+    p = r.report(3, 0.5)
+    print(p)
+
+if __name__ == '__main__':
+  unittest.main()

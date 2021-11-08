3058b174473ec26fade670360c8b416d0c1b1baf
hail-kang <73281731+hail-kang@users.noreply.github.com>
2021-11-08 20:07:04 +0900

    인터페이스 구조에서 추상클래스 구조로 변환

--- neighborhood/similarity.py
+++ neighborhood/similarity.py
@@ -6,24 +6,6 @@ from numpy.linalg import norm
 
 class Similarity(metaclass=abc.ABCMeta):
 
-  @abc.abstractclassmethod
-  def distance(self):
-    pass
-
-  @abc.abstractclassmethod
-  def keys(self):
-    pass
-
-  @abc.abstractclassmethod
-  def values(self):
-    pass
-
-  @abc.abstractclassmethod
-  def report(self):
-    pass
-
-class CosineSimilarity(Similarity):
-
   def __init__(self, origin:dict):
     if not isinstance(origin, dict):
       raise Exception('origin must be dict')
@@ -31,10 +13,10 @@ class CosineSimilarity(Similarity):
       raise Exception('value of origin must be number')
     self.origin = origin
 
-    self.__origin_keys = set(self.keys())
-    self.__other_keys = None
-    self.__origin_values = None
-    self.__other_values = None
+    self._origin_keys = set(self.keys())
+    self._other_keys = None
+    self._origin_values = None
+    self._other_values = None
 
   def keys(self) -> iter:
     return self.origin.keys()
@@ -42,20 +24,30 @@ class CosineSimilarity(Similarity):
   def values(self) -> iter:
     return self.origin.values()
 
-  def distance(self, other) -> float:
-    self.__other_keys = set(other.keys())
-    union_keys = self.__origin_keys | self.__other_keys
-    self.__origin_values = [self.origin[key] if key in self.__origin_keys else 0 for key in union_keys]
-    self.__other_values = [other[key] if key in self.__other_keys else 0 for key in union_keys]
+  @abc.abstractclassmethod
+  def distance(self):
+    pass
 
-    d = (norm(self.__origin_values)*norm(self.__other_values))
+  @abc.abstractclassmethod
+  def report(self):
+    pass
+
+class CosineSimilarity(Similarity):
+    
+  def distance(self, other) -> float:
+    self._other_keys = set(other.keys())
+    union_keys = self._origin_keys | self._other_keys
+    self._origin_values = [self.origin[key] if key in self._origin_keys else 0 for key in union_keys]
+    self._other_values = [other[key] if key in self._other_keys else 0 for key in union_keys]
+
+    d = (norm(self._origin_values)*norm(self._other_values))
     if d == 0:
       return 0
-    return dot(self.__origin_values, self.__other_values)/d
+    return dot(self._origin_values, self._other_values)/d
 
   def report(self, other) -> dict:
     distance = self.distance(other)
-    substract_keys = self.__other_keys - self.__origin_keys
+    substract_keys = self._other_keys - self._origin_keys
     return {
       'distance': distance,
       'key_value': { key: other[key] for key in substract_keys }

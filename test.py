d34057f8c499c100b7a847dbeea94384b9f33f1a
hail-kang <73281731+hail-kang@users.noreply.github.com>
2021-11-09 21:56:34 +0900

    테스트 케이스 수정

--- test.py
+++ test.py
@@ -5,6 +5,18 @@ from neighborhood import *
 class RecommendTest(unittest.TestCase):
 
   def test_cosine_smallcase(self):
+    print('=====test_euclidean_smallcase=====')
+    s = EuclideanSimilarity({'a': 2, 'b': 1, 'c': 5})
+    n = Neighborhood(s, 11)
+    r = RatioRecommend(n)
+    r.add({'a': 2, 'b': 1, 'c': 5, 'd': 4})
+    r.add({'a': 3, 'b': 1, 'c': 5, 'd': 4})
+    r.add({'a': 2, 'b': 2, 'c': 5, 'd': 3})
+    p = r.report(3, 0.5)
+    print(p)
+    print('==============end=============')
+
+  def test_cosine_smallcase(self):
     print('=====test_cosine_smallcase=====')
     s = CosineSimilarity({'a': 2, 'b': 1, 'c': 5})
     n = Neighborhood(s, 0.5)
